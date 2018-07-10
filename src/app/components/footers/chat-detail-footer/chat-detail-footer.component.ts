31592a3dee2fb381e8f66eeb3fa47a3db4b62a5c
Ones <33573384+NoBodyL@users.noreply.github.com>
2018-07-10 18:39:08 +0800

    临时

---
+++ src/app/components/footers/chat-detail-footer/chat-detail-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-detail-footer',
+  templateUrl: './chat-detail-footer.component.html',
+  styleUrls: ['./chat-detail-footer.component.scss']
+})
+export class ChatDetailFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/pages/chat-detail/chat-detail.module.ts
+++ src/app/pages/chat-detail/chat-detail.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { ChatDetailComponent } from './chat-detail.component';
 import { ChatDetailRoutingModule } from './chat-detail.routing.module';
 import { SharedModule } from '../../components/index';
+import { ChatDetailFooterComponent } from '../../components/footers/chat-detail-footer/chat-detail-footer.component';
 //TODO:*ngFor 没有被识别一般就是因为CommonModule没有被导入该module
 // Property binding ngForOf not used by any directive on an embedded template.
 import { CommonModule } from '@angular/common';
@@ -12,7 +13,8 @@ import { CommonModule } from '@angular/common';
     CommonModule
   ],
   declarations: [
-    ChatDetailComponent
+    ChatDetailComponent,
+    ChatDetailFooterComponent
   ]
 })
 export class ChatDetailModule { }
