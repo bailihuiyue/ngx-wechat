74c70db959e37b5e5a4e568cd73520392eec15d5
Ones <33573384+bailihuiyue@users.noreply.github.com>
2018-07-11 21:57:01 +0800

    初步完成表情面板的制作

--- src/app/components/footers/chat-detail-footer/chat-detail-footer.component.ts
+++ src/app/components/footers/chat-detail-footer/chat-detail-footer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, HostListener,ElementRef,ViewChild } from '@angular/core';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'app-chat-detail-footer',
   templateUrl: './chat-detail-footer.component.html',
@@ -9,16 +9,16 @@ export class ChatDetailFooterComponent implements OnInit {
 
   constructor() { }
   @HostListener('touchstart', ['$event.target']) touchstart(btn) {
-    $(btn).parent(".voice,.more,.face").css("background-color", "#F0F0F0");
+    $(btn).parent(".voice,.more,.face").css("background-color", "#E2E2E2");
     if ($(btn).hasClass("talk")) {
-      $(btn).css("background-color", "#F0F0F0").text("松开 结束");
+      $(btn).css("background-color", "#E2E2E2").text("松开 结束");
       this.showMicroPhone = true;
     }
   }
   @HostListener('touchend', ['$event.target']) touchend(btn) {
-    $(btn).parent(".voice,.more,.face").css("background-color", "#FFFFFF");
+    $(btn).parent(".voice,.more,.face").css("background-color", "transparent");
     if ($(btn).hasClass("talk")) {
-      $(btn).css("background-color", "#FFFFFF").text("按住 说话");
+      $(btn).css("background-color", "transparent").text("按住 说话");
       this.showMicroPhone = false;
     }
   }
@@ -28,7 +28,10 @@ export class ChatDetailFooterComponent implements OnInit {
 
   //显示说话时的效果(屏幕中间的话筒)
   showMicroPhone: boolean = false;
-  pic:string="voice";
+  voicePic:string="voice";
+  height:number=0;
+  isShowEmoji:boolean=false;
+  emojiPic:string="face";
   @ViewChild('txt') inputTxt: ElementRef;
   ngOnInit() {
 
@@ -40,11 +43,28 @@ export class ChatDetailFooterComponent implements OnInit {
   changeTxt() {
     this.isTalk = !this.isTalk;
     if(this.isTalk){
-      this.pic="keyboard";
+      this.voicePic="keyboard";
     }else{
-      this.pic="voice";
+      this.voicePic="voice";
       //TODO:problem:不能获取焦点,不知道原因
       this.inputTxt.nativeElement.focus();
     }  
+  }
+
+  //TODO:表情到键盘的按钮切换有点不协调是因为没有找到不带圆圈的笑脸表情图片
+  showEmoji(){
+    this.isShowEmoji=!this.isShowEmoji;
+    if(this.isShowEmoji){
+      this.emojiPic="keyboard";
+      $(".face").addClass("face-keyboard-border");
+    }else{
+      this.emojiPic="face";
+      $(".face").removeClass("face-keyboard-border");
+    }
+    //表情图片的宽与长的比率
+    let rate=0.7456;
+    let width=$("body").width();
+    this.height=width*rate;
+    $(".body,.chat-detail-footer,.lazy").css("transform","translateY(-"+this.height+"px)");
   }
 }
