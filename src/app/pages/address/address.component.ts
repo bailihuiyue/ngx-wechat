f5f8f7bee97867f85ced4222b17e01fdbfed19f7
Ones <33573384+bailihuiyue@users.noreply.github.com>
2018-07-19 21:54:51 +0800

    完成address的布局

--- src/app/pages/address/address.component.ts
+++ src/app/pages/address/address.component.ts
@@ -12,13 +12,25 @@ import { ContactsService } from '../../services/contacts.service';
 export class AddressComponent implements OnInit {
 
   constructor(@Inject('BASE_CONFIG') private config, private contactsService$: ContactsService) { }
-  contact: object = {};
-  imgBaseUrl:string="";
+  contact: Array<any> = [];
+  imgBaseUrl: string = "";
+  anchors: Array<string> = [];
   ngOnInit() {
-    this.imgBaseUrl=this.config.imgBaseUrl;
+    this.imgBaseUrl = this.config.imgBaseUrl;
     this.contactsService$.getUContact().subscribe(data => {
       this.contact = data;
-      console.log(this.contact);
     });
+
+    for (var i = 0; i < 26; i++) {
+      this.anchors.push(String.fromCharCode((65 + i)));
+    }
+    console.log(this.anchors);
+  }
+
+  grey(e) {
+    e.target.style.backgroundColor = "#E2E2E2";
+  }
+  normal(e) {
+    e.target.style.backgroundColor = "#FFFFFF";
   }
 }
